2b362578c59fc69965f54fac036b369b52ee0785
Fernanda Larocca <fernandacolacovaz@hotmail.com>
2023-08-11 15:39:25 -0300

    Fazendo as configurações para que funcione

--- Documentos/Mobile/Calculadora/src/app/home/home.page.ts
+++ Documentos/Mobile/Calculadora/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,18 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  primeiroNumero: string = ''; // Inicializa a variável para o campo de entrada
-  segundoNumero: string = '';
 
-  selecionarOpcao(event: any) {
-    const opcaoSelecionada = event.detail.value;
-    console.log('Opção selecionada:', opcaoSelecionada);
+  constructor(private alertController: AlertController) {
+    this.primeiroNumero = 0;
+    this.segundoNumero = 0;
   }
 
+  primeiroNumero: number;
+  segundoNumero: number;
+  operacaoSelecionada: string = '';
+
   calcular() {
-    console.log('Operação:', this.selecionarOpcao);
-    console.log('Texto digitado:', this.primeiroNumero);
-    console.log('Texto digitado:', this.segundoNumero);
-    
+    const num1 = parseFloat(this.primeiroNumero.toString()); // Converter para número
+    const num2 = parseFloat(this.segundoNumero.toString()); // Converter para número
+
+    if (this.operacaoSelecionada === 'opcao1') {
+      return this.soma(num1, num2);
+    } else if (this.operacaoSelecionada === 'opcao2') {
+      return this.subtracao(num1, num2);
+    } else if (this.operacaoSelecionada === 'opcao3') {
+      return this.multiplicacao(num1, num2);
+    } else if (this.operacaoSelecionada === 'opcao4') {
+      return this.divisao(num1, num2);
+    } else {
+      console.log('Nenhuma operação selecionada.');
+      return ''; // Retornar uma string vazia caso nenhuma operação seja selecionada
+    }
+  }
+
+  soma(num1: number, num2: number) {
+    console.log('Ação 1 executada.');
+    const resultadoSoma: number = num1 + num2;
+    return resultadoSoma.toString(); // Converter o resultado para string
+  }
+
+  subtracao(num1: number, num2: number){
+    console.log('Ação 2 executada.');
+    const resultadoSubtracao: number = num1 - num2;
+    return resultadoSubtracao.toString();
+  }
+
+  multiplicacao(num1: number, num2: number){
+    console.log('Ação 3 executada.');
+    const resultadoMultiplicacao: number = num1 * num2;
+    return resultadoMultiplicacao.toString();
+  }
+
+  divisao(num1: number, num2: number){
+    console.log('Ação 4 executada.');
+    const resultadoDivisao: number = num1 / num2;
+    return resultadoDivisao.toString();
+  }
+
+  async exibirAlerta() {
+    const resultado = this.calcular();
+    const alert = await this.alertController.create({
+      header: 'Calculado!',
+      message: resultado,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
